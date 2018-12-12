c9ba51071002088dff33abb97406a0fc2bc9f384
roman.chepky <roman.chepky@axamit.com>
2018-12-12 17:31:03 +0300

    [service] names [excel] write?

--- src/main/java/frame/ChoseFile.java
+++ src/main/java/frame/ChoseFile.java
@@ -8,39 +8,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.Image;
 import service.ImageService;
 
 import static service.ImageService.getExtensionList;
 
 public class ChoseFile extends Application {
     private Desktop desktop = Desktop.getDesktop();
+    private TableView table = new TableView();
 
     List<File> receivedFiles = new ArrayList<>();
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         final FileChooser fileChooser = new FileChooser();
-
         TextArea textArea = new TextArea();
         textArea.setMinHeight(70);
-
         Button buttonM = new Button("Select Multi Files");
-
-
         buttonM.setOnAction(event -> {
             textArea.clear();
             receivedFiles = fileChooser.showOpenMultipleDialog(primaryStage);
 
+            List<String> nameList = ImageService.getNameList(receivedFiles);
             List<String> extensionList = ImageService.getExtensionList(receivedFiles);
             List<Long> sizeList = ImageService.getSizeList(receivedFiles);
             List<String> resolutionList = ImageService.getResolutionList(receivedFiles);
@@ -48,21 +44,24 @@ public class ChoseFile extends Application {
             List<Integer> colorList = ImageService.getColorList(receivedFiles);
 
 
-            printLog(textArea, receivedFiles);
-        });
 
+
+            printLog(textArea, receivedFiles);
+          //  showResults(primaryStage);
+        });
 
         VBox root = new VBox();
         root.setPadding(new Insets(10));
         root.setSpacing(5);
-
         root.getChildren().addAll(textArea, buttonM);
-
         Scene scene = new Scene(root, 800, 400);
-
-        primaryStage.setTitle("JavaFX FileChooser (o7planning.org)");
+        primaryStage.setTitle("Chepky Roman");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
+
     }
 
     private void printLog(TextArea textArea, List<File> files) {
@@ -71,15 +70,42 @@ public class ChoseFile extends Application {
         }
         for (File file : files) {
             textArea.appendText(file.getAbsolutePath() + "\n");
-
-
-
         }
     }
 
 
 
+    private void showResults(Stage stage){
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Table View Sample");
+        stage.setWidth(300);
+        stage.setHeight(500);
+
+        final Label label = new Label("Address Book");
+        label.setFont(new Font("Arial", 20));
+
+        table.setEditable(true);
+
+        TableColumn firstNameCol = new TableColumn("First Name");
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        TableColumn emailCol = new TableColumn("Email");
+
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
     public static void main(String[] args) {
+
         Application.launch(args);
     }
 }
--- src/main/java/service/ExcelWriter.java
+++ src/main/java/service/ExcelWriter.java
@@ -1,15 +1,43 @@
 package service;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileOutputStream;
+
 public class ExcelWriter {
     public void createFile(){
-        Workbook workbook = new XSSFWorkbook();
-        CreationHelper createHelper = workbook.getCreationHelper();
-        Sheet sheet = workbook.createSheet("FeatureSheet");
+        try {
+            String filename = "C:/NewExcelFile.xls" ;
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("FirstSheet");
 
+            HSSFRow rowhead = sheet.createRow((short)0);
+            rowhead.createCell(0).setCellValue("No.");
+            rowhead.createCell(1).setCellValue("Name");
+            rowhead.createCell(2).setCellValue("Address");
+            rowhead.createCell(3).setCellValue("Email");
+
+            HSSFRow row = sheet.createRow((short)1);
+            row.createCell(0).setCellValue("1");
+            row.createCell(1).setCellValue("Sankumarsingh");
+            row.createCell(2).setCellValue("India");
+            row.createCell(3).setCellValue("sankumarsingh@gmail.com");
+
+
+
+            FileOutputStream fileOut = new FileOutputStream("NewExcelFile.xls");
+            workbook.write(fileOut);
+            fileOut.close();
+            System.out.println("Your excel file has been generated!");
+
+        } catch ( Exception ex ) {
+            System.out.println(ex);
+        }
     }
 }
